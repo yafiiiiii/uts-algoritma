02cc9b1a8065546cabe99b044264eba546d80f60
Yafii <yafiyeah@gmail.com>
2026-05-06 09:01:49 +0700

    uts_algoritma

---
+++ bubble_sort.py
@@ -0,0 +1,18 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # tukar posisi
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+if __name__ == "__main__":
+    data = [5, 2, 9, 1, 5, 6]
+    print("Sebelum:", data)
+    print("Sesudah:", bubble_sort(data))
